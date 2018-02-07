8b3aba9d5327f0bfc681f58bd59d3358a419d400
swon96 <ohsing0906@gmail.com>
2018-02-07 23:09:20 +0900

    작업폴더  생성

---
+++ Debug/main.d
@@ -0,0 +1,21 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/linkage.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/cc430f5123.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/stdarg.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/cc430f5123.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.6.LTS/include/intrinsics_legacy_undefs.h: 
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <msp430.h> 
+
+
+/**
+ * hello.c
+ */
+int main(void)
+{
+
+    printf("Hello World!\n");
+
+    return 0;
+}
